784d7e1d5fd7b80b9318e3c742ecb81425527a01
wankhede04 <wankhedevijay04@gmail.com>
2023-08-22 13:33:17 +0530

    feat: Defined state variable of distributor and uno

--- contracts/proxies/VeYieldDistributorProxy.sol
+++ contracts/proxies/VeYieldDistributorProxy.sol
@@ -7,3 +7,12 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 import "../interfaces/apps/IVeUnoDaoYieldDistributor.sol";
+
+contract NotifyRewardProxy is AccessControl {
+    using SafeERC20 for IERC20;
+
+    bytes32 public constant EXECUTOR_ROLE = keccak256("EXECUTOR_ROLE");
+
+    IVeUnoDaoYieldDistributor public yieldDistributor;
+    IERC20 public uno;
+}
