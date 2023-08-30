8d82e095cb063f8aa859e2340b61f7ceab0a1b3b
wankhede04 <wankhedevijay04@gmail.com>
2023-08-30 17:31:23 +0530

    test: add MockVeUno contract

---
+++ contracts/mocks/MockVeUno.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity 0.8.10;
+
+// import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+// import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MockVeUno {
+
+    function totalSupply() external returns(uint256){
+        return 10000000000000000000000000000000000000000;
+    }
+}
