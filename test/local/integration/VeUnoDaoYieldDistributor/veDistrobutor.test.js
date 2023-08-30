122714fa3526b28061ced51d0702ee772e6b747b
wankhede04 <wankhedevijay04@gmail.com>
2023-08-30 17:32:00 +0530

    feat: add unit test for notifyReward

--- test/local/integration/VeUnoDaoYieldDistributor/veDistrobutor.test.js
+++ test/local/integration/VeUnoDaoYieldDistributor/veDistrobutor.test.js
@@ -30,12 +30,14 @@ describe("VeUnoDaoYieldDistributor", function() {
     this.Token = await ethers.getContractFactory("MockUno");
     this.VotingEscrow = await ethers.getContractFactory("VotingEscrow");
     this.VeUnoDaoYieldDistributor = await ethers.getContractFactory("VeUnoDaoYieldDistributor");
+    this.MockVeUno = await ethers.getContractFactory("MockVeUno");
 
     this.ownership = await this.Ownership.deploy();
     this.token = await this.Token.deploy(name, symbol);
     this.voting_escrow = await this.VotingEscrow.deploy(
       this.token.address, "Voting-escrowed UnoRe", "veUnoRe", "1", this.ownership.address
     );
+    this.mockVeUno = await this.MockVeUno.deploy();
 
     this.veUnoDaoYieldDistributor = await this.VeUnoDaoYieldDistributor.deploy(
       this.token.address,
@@ -57,6 +59,18 @@ describe("VeUnoDaoYieldDistributor", function() {
 
     //setup
     this.escrowedAmount = BigNumber.from(1000).mul(BigNumber.from(10).pow(18));
+
+    this.token.faucet(ten_to_the_40);
+
+    this.NotifyRewardProxy = await ethers.getContractFactory(
+      "NotifyRewardProxy"
+    );
+    this.notifyRewardProxy = await this.NotifyRewardProxy.deploy(
+      this.veUnoDaoYieldDistributor.address,
+      this.token.address,
+      this.mockVeUno.address,
+      this.creator.address
+    );
   });
 
   it("veToken distributor flow", async function() {
@@ -95,5 +109,49 @@ describe("VeUnoDaoYieldDistributor", function() {
     // await this.veUnoDaoYieldDistributor.connect(this.alice).getYield();
     // aliceUNOBalance = await this.token.balanceOf(this.alice.address);
     // console.log('Alice UNO Balance ==>', aliceUNOBalance.toString());
-  })
-})
+  });
+
+  describe("NotifyRewardProxy", function () {
+    it("Should transfer reward amount to VeUnoDaoYieldDistributor", async function () {
+      await this.notifyRewardProxy.updateApy(6000);
+      const EXECUTOR_ROLE = await this.notifyRewardProxy.EXECUTOR_ROLE();
+      await this.notifyRewardProxy.grantRole(EXECUTOR_ROLE,this.creator.address);
+      await this.veUnoDaoYieldDistributor.toggleRewardNotifier(this.notifyRewardProxy.address);
+
+      let amount = await this.notifyRewardProxy.getRewardAmount();
+      await this.token.approve(this.notifyRewardProxy.address, amount);
+      let balaceBefore = await this.token.balanceOf(this.veUnoDaoYieldDistributor.address);
+
+      const tx = await this.notifyRewardProxy.execNotifyReward(this.creator.address);
+      const blockNum = await ethers.provider.getBlockNumber();
+      const block = await ethers.provider.getBlock(blockNum);
+      const expectedCurrentTimestamp = block.timestamp;
+      let balaceAfter = await this.token.balanceOf(this.veUnoDaoYieldDistributor.address);
+      await expect(tx).to.emit(this.notifyRewardProxy, "NotifyRewardExecuted").withArgs(this.creator.address, amount);
+
+      expect(await this.veUnoDaoYieldDistributor.lastUpdateTime()).to.equal(BigNumber.from(expectedCurrentTimestamp));
+
+      let yieldDuration = await this.veUnoDaoYieldDistributor.yieldDuration();
+      let expectedPeriodFinish = BigNumber.from(expectedCurrentTimestamp).add(yieldDuration);
+      expect(await this.veUnoDaoYieldDistributor.periodFinish()).to.equal(expectedPeriodFinish);
+      expect(balaceAfter).to.equal(balaceBefore.add(BigNumber.from(amount)));
+    });
+
+    it("Should revert when caller has no executor role", async function () {
+      await this.notifyRewardProxy.updateApy(6000);
+      const EXECUTOR_ROLE = await this.notifyRewardProxy.EXECUTOR_ROLE();
+      await expect(this.notifyRewardProxy.execNotifyReward(this.creator.address)).to.be.revertedWith(
+        `AccessControl: account ${this.creator.address.toLowerCase()} is missing role ${EXECUTOR_ROLE}`
+      );
+    });
+
+    it("Should revert when notifyRewardProxy is not whitelisted in veUnoDaoYieldDistributor", async function () {
+      await this.notifyRewardProxy.updateApy(6000);
+      let amount = await this.notifyRewardProxy.getRewardAmount();
+      await this.token.approve(this.notifyRewardProxy.address, amount);
+      const EXECUTOR_ROLE = await this.notifyRewardProxy.EXECUTOR_ROLE();
+      await this.notifyRewardProxy.grantRole(EXECUTOR_ROLE,this.creator.address);
+      await expect(this.notifyRewardProxy.execNotifyReward(this.creator.address)).to.be.revertedWith("Sender not whitelisted");
+    });
+  });
+});
