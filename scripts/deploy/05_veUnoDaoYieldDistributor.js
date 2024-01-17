95ddf206f2295526ab6f8ada10482567e0f05e40
JinDev627 <113541562+JinDev627@users.noreply.github.com>
2024-01-17 20:40:40 +0700

    deploy the uno dao

--- scripts/deploy/05_veUnoDaoYieldDistributor.js
+++ scripts/deploy/05_veUnoDaoYieldDistributor.js
@@ -4,20 +4,16 @@ module.exports = async function ({ ethers, getNamedAccounts, deployments, getCha
     const { deploy } = deployments;
     const { deployer } = await getNamedAccounts();
   
-    this.mockUno = await deployments.get('MockUno');
-    const mockUnoAddress = '0x3a554dc1EAc143FC4640b0294A342B4F9089FDE6' // this.mockUno.address;
+    const mockUno = await deployments.get('MockUno');
+    const mockUnoAddress = mockUno.address;
     const timeLockAddress = '0x7e47419EFE3E49f3E616965bFa96f089b2b0e574';
-    // const votingEscrow = await deployments.get("VotingEscrow");
-    const votingEscrowAddress = '0xE55A92fa510e2d277E4cF947189D0523683D63EC'; // votingEscrow.address;
+    const votingEscrow = await deployments.get("VotingEscrow");
+    const votingEscrowAddress = votingEscrow.address;
   
     await deploy('VeUnoDaoYieldDistributor', {
       from: deployer,
       log: true,
-      args: [
-        mockUnoAddress,
-        timeLockAddress,
-        votingEscrowAddress
-      ],
+      args: [],
       deterministicDeployment: false
     });
   };
