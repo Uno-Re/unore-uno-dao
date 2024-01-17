0cbae00cb502d605132e417c3442bcffca897087
JinDev627 <113541562+JinDev627@users.noreply.github.com>
2024-01-17 20:47:26 +0700

    deploy the uno dao

---
+++ scripts/deploy/04_votingEscrowOld.js
@@ -0,0 +1,38 @@
+// Defining bytecode and abi from original contract on mainnet to ensure bytecode matches and it produces the same pair code hash
+
+module.exports = async function ({ ethers, getNamedAccounts, deployments, getChainId }) {
+    const { deploy } = deployments;
+    const { deployer } = await getNamedAccounts();
+
+    // address _token_addr,
+    //     string memory _name,
+    //     string memory _symbol,
+    //     string memory _version,
+    //     address _ownership
+  
+    this.mockUno = await deployments.get('MockUno');
+    const mockUnoAddress = this.mockUno.address;
+    const _name = 'UnoDao Voting Old';
+    const _symbol = 'UnoDaoVE';
+    const _version = '1';
+
+    this.ownership = await deployments.get('Ownership');
+    const ownershipAddress = this.ownership.address;
+  
+    await deploy('VotingEscrowOld', {
+      from: deployer,
+      log: true,
+      args: [
+        mockUnoAddress,
+        _name,
+        _symbol,
+        _version,
+        ownershipAddress
+      ],
+      deterministicDeployment: false
+    });
+  };
+  
+  module.exports.tags = ['VotingEscrowOld', 'UnoDao'];
+  module.exports.dependencies = ['MockUno', 'MockUno'];
+  
