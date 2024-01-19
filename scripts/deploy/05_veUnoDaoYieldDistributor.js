fc36515111892618302a2ecabc45addb0bfbf093
JinDev627 <113541562+JinDev627@users.noreply.github.com>
2024-01-19 20:51:38 +0700

    deploy the veUno

--- scripts/deploy/05_veUnoDaoYieldDistributor.js
+++ scripts/deploy/05_veUnoDaoYieldDistributor.js
@@ -9,13 +9,24 @@ module.exports = async function ({ ethers, getNamedAccounts, deployments, getCha
     const timeLockAddress = '0x7e47419EFE3E49f3E616965bFa96f089b2b0e574';
     const votingEscrow = await deployments.get("VotingEscrow");
     const votingEscrowAddress = votingEscrow.address;
+    const owner = "0xB4828FBf7753Ade73B608604690128e1FD1e9d3B";
   
-    await deploy('VeUnoDaoYieldDistributor', {
+    const veUnoDao = await deploy("VeUnoDaoYieldDistributor", {
       from: deployer,
+      contract: "VeUnoDaoYieldDistributor",
       log: true,
-      args: [],
-      deterministicDeployment: false
-    });
+      proxy: {
+        execute: {
+          init: {
+            methodName: "initialize",
+            args: [mockUnoAddress, votingEscrowAddress, timeLockAddress, owner],
+          },
+        },
+        proxyContract: "OpenZeppelinTransparentProxy",
+      },
+    })
+  
+    console.log(`deploy at ${veUnoDao.address}`)
   };
   
   module.exports.tags = ['VeUnoDaoYieldDistributor', 'UnoDao'];
